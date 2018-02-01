a17d1cdaaa1a543d7b106c071c3ae933c5d429b1
Zeacone <andreboot42@gmail.com>
2018-02-02 07:34:14 +0800

    Update README.

---
+++ src/SVGFilter.swift
@@ -0,0 +1,13 @@
+//
+//  SVGFilter.swift
+//  Demo
+//
+//  Created by Zeacone on 2018/1/26.
+//  Copyright © 2018年 zeacone. All rights reserved.
+//
+
+import UIKit
+
+class SVGFilter: NSObject {
+
+}
---
+++ src/SVGGradient.swift
@@ -0,0 +1,13 @@
+//
+//  SVGGradient.swift
+//  Demo
+//
+//  Created by Zeacone on 2018/1/26.
+//  Copyright © 2018年 zeacone. All rights reserved.
+//
+
+import UIKit
+
+class SVGGradient: NSObject {
+
+}
